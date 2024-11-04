a81a2734aba710f1d2c1ecd7bc3e3a76d1aba6c8
EsTaNG9 <jpserrada@hotmail.com>
2024-11-04 21:26:48 +0000

    a1

---
+++ FREERTOS_ECU.ino
@@ -0,0 +1,98 @@
+#include "Arduino.h"
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_task_wdt.h"
+
+/* The tasks to be created. */
+void calculateRPM( void *pvParameters );
+
+/* The service routine for the interrupt.  This is the interrupt that the task
+will be synchronized with. */
+void IRAM_ATTR onPulse( void );
+
+/*-----------------------------------------------------------*/
+
+/* Declare a variable of type SemaphoreHandle_t.  This is used to reference the
+semaphore that is used to synchronize a task with an interrupt. */
+SemaphoreHandle_t xBinarySemaphore;
+
+// pin to generate interrupts
+const uint8_t interruptPin = 4;
+
+volatile unsigned long pulseCount = 0; // Counts pulses
+unsigned long lastTime = 0; // Last time RPM was calculated
+unsigned int rpm = 0; // RPM value
+
+
+void setup( void )
+{
+  // Set loopTask max priority before deletion
+  vTaskPrioritySet(NULL, configMAX_PRIORITIES-1);
+
+  // Init USART and set Baud-rate to 115200
+  Serial.begin(115200);
+    /* Before a semaphore is used it must be explicitly created.  In this example
+  a binary semaphore is created. */
+    vSemaphoreCreateBinary( xBinarySemaphore );
+
+
+   pinMode(interruptPin, INPUT_PULLUP);
+   attachInterrupt(digitalPinToInterrupt(interruptPin), vExampleInterruptHandler, FALLING);
+
+
+
+  /* Check the semaphore was created successfully. */
+  if( xBinarySemaphore != NULL )
+  {
+    /* Create the 'handler' task.  This is the task that will be synchronized
+    with the interrupt.  The handler task is created with a high priority to
+    ensure it runs immediately after the interrupt exits.  In this case a
+    priority of 3 is chosen. */
+    xTaskCreatePinnedToCore( calculateRPM, "Calculate RPM", 1024, NULL, 3, NULL, 1);
+
+  }
+
+    /* If all is well we will never reach here as the scheduler will now be
+    running the tasks.  If we do reach here then it is likely that there was
+    insufficient heap memory available for a resource to be created. */
+//  for( ;; );
+//  return 0;
+}
+
+// Task to calculate RPM
+void calculateRPM(void *pvParameters) {
+    while (true) {
+        // Calculate RPM every second
+        unsigned long currentTime = millis();
+        unsigned long interval = currentTime - lastTime;
+
+        if (interval >= 1000) { // Calculate every 1000ms (1 second)
+            rpm = (pulseCount * 60 * 1000) / interval; // RPM calculation
+            pulseCount = 0; // Reset pulse count
+            lastTime = currentTime;
+
+            Serial.print("RPM: ");
+            Serial.println(rpm); // Output RPM
+        }
+
+        vTaskDelay( 100 / portTICK_PERIOD_MS); // Delay for task scheduling
+    }
+}
+
+
+void IRAM_ATTR onPulse( void )
+{
+
+	//Serial.print( "interrpcao\r\n" );
+	pulseCount++; // Increase pulse count on each pulse
+
+
+}
+//------------------------------------------------------------------------------
+void loop()
+{
+  vTaskDelete( NULL );
+}
