8156a0fe389d4dbad0e95bebd6d8d518eaeacae8
João Serrada <jpserrada@hotmail.com>
2024-12-05 17:18:12 +0000

    a14 sinal de vida
    interrupcao a funfar e getRPM tb a funfar

--- FREERTOS_ECU.ino
+++ FREERTOS_ECU.ino
@@ -22,9 +22,6 @@
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK,
 TFT_RST, TFT_MISO);
 
-/* The tasks to be created. */
-void vInitDisplay(void *pvParameters);
-
 /* The service routine for the interrupt.  This is the interrupt that the task
  will be synchronized with. */
 void IRAM_ATTR onPulse(void);
@@ -32,24 +29,36 @@ void IRAM_ATTR onPulse(void);
 /* The tasks to be created. */
 //void calculateRPM(void *pvParameters);
 void vInitDisplay(void *pvParameters);
+void vGetLowRPM(void *pvParameters);
+//void vGetCrankAngle(void *pvParameters);
+
+//Handles
+QueueHandle_t xRPM;
+QueueHandle_t xContadorDentes;
+QueueHandle_t xtempoUltimoDente;
 
 // pin to generate interrupts
 const uint8_t interruptPin = 4;
 
 //Configurar roda fonica
 uint16_t numRealDentes = 35;
+uint16_t anguloPorDente = 360 / 36;
+uint16_t desvioPrimeiroDenteTDC = 180; //Desvio real do TDC 1ºcil do primeiro dente da roda
 
 volatile unsigned long tempoAtual = 0;
 volatile unsigned long falhaAtual = 0;
-volatile unsigned long tempoUltimoDente = 0;
+unsigned long tempoUltimoDente = 0; // n pode ser volatile pq quero mandar valores para a queeue
 volatile unsigned long tempoFiltro = 0;
 volatile unsigned long tempoPenultimoDente = 0;
 volatile unsigned long falhaObjetivo = 0;
-volatile uint16_t contadorDentes = 0;
+uint16_t contadorDentes = 0; // n pode ser volatile pq quero mandar valores para a queeue
 volatile unsigned long tempoPrimeiroDenteMenosUm = 0;
 volatile unsigned long tempoPrimeiroDente = 0;
+unsigned long tempoPorGrau = 0; // n pode ser volatile pq quero mandar valores para a queeue
 bool sincronizacao = false;
 volatile uint32_t contadorrevolucoes = 0;
+uint64_t tempoEntreDentes = 0;
+unsigned long tempoDecorrido = 0;
 
 void setup(void) {
 	// Set loopTask max priority before deletion
@@ -60,19 +69,111 @@ void setup(void) {
 
 	//Interrupção Hall Sensor
 	pinMode(interruptPin, INPUT);
-	attachInterrupt(digitalPinToInterrupt(interruptPin), onPulse, FALLING);
+	attachInterrupt(digitalPinToInterrupt(interruptPin), onPulse, RISING);
 
-	xTaskCreatePinnedToCore(vInitDisplay, "Display Boot", 2048, NULL, 1,
-	NULL, 1);
+	xRPM = xQueueCreate(5, sizeof(uint16_t));
+	xContadorDentes = xQueueCreate(5, sizeof(uint8_t));
+	xtempoUltimoDente = xQueueCreate(5, sizeof(long));
+
+	xTaskCreatePinnedToCore(vInitDisplay, "Display Boot", 2048, NULL, 1, NULL,
+			1);
+	xTaskCreatePinnedToCore(vGetLowRPM, "Get low rpm", 2048, NULL, 1, NULL, 1);
+	//xTaskCreatePinnedToCore(vGetCrankAngle, "Get crank angle", 2048, NULL, 1, NULL, 1);
 
 }
 
+/*void vGetCrankAngle(void *pvParameters) {
+ unsigned long temp_tempoUltimoDente;
+ int temp_contadorDentes;
+
+ // Prioridade máxima
+ xQueuePeek(xContadorDentes, &temp_contadorDentes,
+ portMAX_DELAY);
+ xQueuePeek(xtempoUltimoDente, &temp_tempoUltimoDente,
+ portMAX_DELAY);
+ //Prioridade normal
+
+ int anguloCambota = ((temp_contadorDentes - 1) * anguloPorDente)
+ + desvioPrimeiroDenteTDC;
+
+ //Estimar o num de graus desde o ultimo dente
+ tempoDecorrido = micros() - temp_tempoUltimoDente;
+ tempoPorGrau = (temp_tempoUltimoDente / anguloPorDente); //Se n funcionar faço apartir da rpm
+ anguloCambota = anguloCambota + ldiv(tempoDecorrido, tempoPorGrau).quot;
+
+ if (anguloCambota > 360) {
+ anguloCambota = anguloCambota - 360;
+ }
+ }*/
+
+void vGetLowRPM(void *pvParameters) {
+	while (true) {
+		//Maxima prioridade ou desativar interrupcoes
+		//taskDISABLE_INTERRUPTS();
+		//tempoEntreDentes = (tempoUltimoDente - tempoPenultimoDente);
+		tempoEntreDentes = (tempoUltimoDente - tempoPenultimoDente);
+		uint64_t rpm_temporary =
+				(((60000000 / (tempoEntreDentes * 36)) + 1) * 2); //+1 pq falha, n sei pk x2??
+		//taskENABLE_INTERRUPTS();
+		//Prioridade normal
+		xQueueSendToFront(xRPM, &rpm_temporary, 0);
+
+		Serial.println(rpm_temporary);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
+	}
+}
+
+// Interrupção gerada na transição do sinal do sensor de Hall
+void IRAM_ATTR onPulse(void) {
+
+	//Serial.println(falhaAtual);
+	BaseType_t xHigherPriorityTaskWoken;
+
+	tempoAtual = micros();
+	falhaAtual = tempoAtual - tempoUltimoDente;
+
+	if (falhaAtual < tempoFiltro) {
+		//return; //Debounce
+		//Serial.println("Debounce");
+	} else {
+
+		//Serial.print("Tempo Falha: ");
+		//Serial.println(falhaAtual);
+		//Add para guardar, queue aberturaAtual
+
+		falhaObjetivo = 1.5 * (tempoUltimoDente - tempoPenultimoDente);
+
+		if (falhaAtual > falhaObjetivo || contadorDentes > numRealDentes) {
+			contadorDentes = 1;
+			tempoPrimeiroDenteMenosUm = tempoPrimeiroDente;
+			tempoPrimeiroDente = tempoAtual;
+			sincronizacao = true;
+			contadorrevolucoes++;
+		} else {
+			tempoFiltro = falhaAtual * 0.25; // 25% de filtro
+			contadorDentes++;
+			//Serial.print("Dente: ");
+			//Serial.println(contadorDentes);
+			xQueueSendToFrontFromISR(xContadorDentes, &contadorDentes,
+					&xHigherPriorityTaskWoken);
+		}
+
+		tempoPenultimoDente = tempoUltimoDente;
+		tempoUltimoDente = tempoAtual;
+		//xQueueSendToFrontFromISR(xtempoUltimoDente, &tempoUltimoDente, &xHigherPriorityTaskWoken);
+
+		//Serial.println(contadorrevolucoes);
+	}
+
+//Serial.println("int");
+}
+
 void vInitDisplay(void *pvParameters) {
-	// Inicializar o tft
+// Inicializar o tft
 	tft.begin();
-	// Colocar fundo preto
+// Colocar fundo preto
 	tft.fillScreen(ILI9341_BLACK);
-	// Definir orientação da escrita
+// Definir orientação da escrita
 	tft.setRotation(0);
 
 	for (;;) {
@@ -85,34 +186,6 @@ void vInitDisplay(void *pvParameters) {
 
 		vTaskDelay(500 / portTICK_PERIOD_MS);
 	}
-}
-
-// Interrupção gerada na transição do sinal do sensor de Hall
-void IRAM_ATTR onPulse(void) {
-
-	tempoAtual = esp_timer_get_time();
-	falhaAtual = tempoAtual - tempoUltimoDente;
-
-	if (falhaAtual < tempoFiltro) {
-		return; //Debounce
-	}
-
-	//Add queue aberturaAtual
-
-	falhaObjetivo = 1.5 * (tempoUltimoDente - tempoPenultimoDente);
-
-	if (falhaAtual < falhaObjetivo || contadorDentes > numRealDentes) {
-		contadorDentes = 1;
-		tempoPrimeiroDenteMenosUm = tempoPrimeiroDente;
-		tempoPrimeiroDente = tempoAtual;
-		sincronizacao = true;
-		contadorrevolucoes++;
-	} else {
-		tempoFiltro = falhaAtual * 0.25; // 25% de filtro
-	}
-
-	tempoPenultimoDente = tempoUltimoDente;
-	tempoUltimoDente = tempoAtual;
 }
 
 //------------------------------------------------------------------------------
