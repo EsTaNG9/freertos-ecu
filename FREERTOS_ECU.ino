c1dfaaa49c0ec41b4013ddc7f0135b84096841ce
João Serrada <jpserrada@hotmail.com>
2024-12-09 02:47:48 +0000

    a15 not crashing
    progresso omg

--- FREERTOS_ECU.ino
+++ FREERTOS_ECU.ino
@@ -10,6 +10,14 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 
+#define TEMPO_MAXIMO_MOTOR_MORRER 1000
+#define ADC_RESOLUTION 6
+#define VREF_PLUS  5
+#define VREF_MINUS  0.0
+
+#define DEBUG "ON"
+#define DEBUG_LEVEL 2
+
 // Definir os terminais do LCD
 #define TFT_CS   05
 #define TFT_DC   26
@@ -29,16 +37,23 @@ void IRAM_ATTR onPulse(void);
 /* The tasks to be created. */
 //void calculateRPM(void *pvParameters);
 void vInitDisplay(void *pvParameters);
-void vGetLowRPM(void *pvParameters);
-//void vGetCrankAngle(void *pvParameters);
+void vBrain(void *pvParameters);
+void vGetMAP(void *pvParameters);
+
+//Declarar funcoes
+int getCrankAngle(int);
+void getLowRPM(void);
 
 //Handles
 QueueHandle_t xRPM;
 QueueHandle_t xContadorDentes;
 QueueHandle_t xtempoUltimoDente;
+QueueHandle_t xMAP;
 
 // pin to generate interrupts
 const uint8_t interruptPin = 4;
+//const uint8_t bombaCombustivelPin = 28;
+const uint8_t MAPSensorPin = 36;
 
 //Configurar roda fonica
 uint16_t numRealDentes = 35;
@@ -46,8 +61,11 @@ uint16_t anguloPorDente = 360 / 36;
 uint16_t desvioPrimeiroDenteTDC = 180; //Desvio real do TDC 1ºcil do primeiro dente da roda
 
 volatile unsigned long tempoAtual = 0;
+volatile unsigned long tempoAtual_Brain = 1; // Para n dar erro no startup
 volatile unsigned long falhaAtual = 0;
+volatile unsigned long loopAtual_Brain = 1; // Para n dar erro no startup
 unsigned long tempoUltimoDente = 0; // n pode ser volatile pq quero mandar valores para a queeue
+unsigned long tempoUltimoloop_Brain = 0; // n pode ser volatile pq quero mandar valores para a queeue
 volatile unsigned long tempoFiltro = 0;
 volatile unsigned long tempoPenultimoDente = 0;
 volatile unsigned long falhaObjetivo = 0;
@@ -58,7 +76,10 @@ unsigned long tempoPorGrau = 0; // n pode ser volatile pq quero mandar valores p
 bool sincronizacao = false;
 volatile uint32_t contadorrevolucoes = 0;
 uint64_t tempoEntreDentes = 0;
-unsigned long tempoDecorrido = 0;
+uint64_t atualRPS = 0;
+uint64_t atualRPM = 0;
+bool bombaCombustivel = false;
+//unsigned long tempoDecorrido = 0;
 
 void setup(void) {
 	// Set loopTask max priority before deletion
@@ -67,66 +88,131 @@ void setup(void) {
 	// Init USART and set Baud-rate to 115200
 	Serial.begin(115200);
 
+	//setup adc
+	analogReadResolution(ADC_RESOLUTION);
+
 	//Interrupção Hall Sensor
 	pinMode(interruptPin, INPUT);
+	pinMode(MAPSensorPin, OUTPUT);
 	attachInterrupt(digitalPinToInterrupt(interruptPin), onPulse, RISING);
 
 	xRPM = xQueueCreate(5, sizeof(uint16_t));
 	xContadorDentes = xQueueCreate(5, sizeof(uint8_t));
 	xtempoUltimoDente = xQueueCreate(5, sizeof(long));
+	xMAP = xQueueCreate(5, sizeof(float));
 
-	xTaskCreatePinnedToCore(vInitDisplay, "Display Boot", 2048, NULL, 1, NULL,
-			1);
-	xTaskCreatePinnedToCore(vGetLowRPM, "Get low rpm", 2048, NULL, 1, NULL, 1);
-	//xTaskCreatePinnedToCore(vGetCrankAngle, "Get crank angle", 2048, NULL, 1, NULL, 1);
+	xTaskCreatePinnedToCore(vInitDisplay, "vInitDisplay", 2048, NULL, 1, NULL, 1);
+	xTaskCreatePinnedToCore(vBrain, "vBrain", 4096, NULL, 1, NULL, 1);
+	xTaskCreatePinnedToCore(vGetMAP, "vGetMAP", 512, NULL, 1, NULL, 1);
+
+	if (DEBUG == "ON" && DEBUG_LEVEL <= 3) {
+		Serial.println("Setup acabou de correr");
+	}
 
 }
 
-/*void vGetCrankAngle(void *pvParameters) {
- unsigned long temp_tempoUltimoDente;
- int temp_contadorDentes;
-
- // Prioridade máxima
- xQueuePeek(xContadorDentes, &temp_contadorDentes,
- portMAX_DELAY);
- xQueuePeek(xtempoUltimoDente, &temp_tempoUltimoDente,
- portMAX_DELAY);
- //Prioridade normal
-
- int anguloCambota = ((temp_contadorDentes - 1) * anguloPorDente)
- + desvioPrimeiroDenteTDC;
-
- //Estimar o num de graus desde o ultimo dente
- tempoDecorrido = micros() - temp_tempoUltimoDente;
- tempoPorGrau = (temp_tempoUltimoDente / anguloPorDente); //Se n funcionar faço apartir da rpm
- anguloCambota = anguloCambota + ldiv(tempoDecorrido, tempoPorGrau).quot;
-
- if (anguloCambota > 360) {
- anguloCambota = anguloCambota - 360;
- }
- }*/
-
-void vGetLowRPM(void *pvParameters) {
+void vBrain(void *pvParameters) {
 	while (true) {
-		//Maxima prioridade ou desativar interrupcoes
-		//taskDISABLE_INTERRUPTS();
-		//tempoEntreDentes = (tempoUltimoDente - tempoPenultimoDente);
-		tempoEntreDentes = (tempoUltimoDente - tempoPenultimoDente);
-		uint64_t rpm_temporary =
-				(((60000000 / (tempoEntreDentes * 36)) + 1) * 2); //+1 pq falha, n sei pk x2??
-		//taskENABLE_INTERRUPTS();
-		//Prioridade normal
-		xQueueSendToFront(xRPM, &rpm_temporary, 0);
+		unsigned long temp_tempoUltimoDente;
 
-		Serial.println(rpm_temporary);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
+		//loopAtual_Brain = tempoAtual_Brain - tempoUltimoloop_Brain;
+		loopAtual_Brain = tempoAtual_Brain;
+		tempoAtual_Brain = micros();
+
+		//xQueuePeek(xtempoUltimoDente, &temp_tempoUltimoDente, 0);
+		//Serial.println(temp_tempoUltimoDente);
+		//unsigned long tempoParaUltimoDente = (loopAtual_Brain - temp_tempoUltimoDente);
+		unsigned long tempoParaUltimoDente = (loopAtual_Brain - tempoUltimoDente);
+
+		//Serial.println(tempoParaUltimoDente);
+
+		if ((tempoParaUltimoDente < TEMPO_MAXIMO_MOTOR_MORRER) || (tempoParaUltimoDente > loopAtual_Brain)) {
+			int ultimaRPM = atualRPM;
+			getLowRPM();
+			xQueuePeek(xRPM, &atualRPM, (TickType_t) 10); // Para calcular RPS
+			if (bombaCombustivel == false) {
+				//digitalWrite(bombaCombustivel, HIGH);
+				bombaCombustivel = true;
+			} //Check if the fuel pump is on and turn it on if it isn't.
+			atualRPS = ldiv(1000000, (loopAtual_Brain - tempoUltimoloop_Brain)).quot * (atualRPM - ultimaRPM); //This is the RPM per second that the engine has accelerated/decelleratedin the last loop
+		} else {
+			atualRPM = 0;
+			contadorrevolucoes = 0;
+			//bombaCombustivel = false;
+			sincronizacao = false;
+			atualRPS = 0;
+		}
+
+		if (sincronizacao && (atualRPM > 0)) {
+
+		}
+
+		//int anguloCambota = getCrankAngle(tempoPorGrau);
+
+		/*Serial.println(sincronizacao ? "true" : "false");
+
+		 Serial.print("atualRPS: ");
+		 Serial.println(atualRPS);
+
+		 Serial.print("anguloCambota: ");
+		 Serial.println(anguloCambota);
+
+		 Serial.print("contadorrevolucoes: ");
+		 Serial.println(contadorrevolucoes);*/
+
+		if ( DEBUG == "ON" && DEBUG_LEVEL <= 2) {
+			Serial.print("Correu a task: ");
+			Serial.println(pcTaskGetTaskName(NULL));
+		}
+		vTaskDelay(5 / portTICK_PERIOD_MS);
 	}
+}
+
+/* Nota: n estou a conseguir implementar as queeue peeks sem crashar cpu*/
+int getCrankAngle(int tempoPorGrau) {
+	unsigned long temp_tempoUltimoDente;
+	int temp_contadorDentes;
+
+// Prioridade máxima
+	//xQueuePeek(xContadorDentes, &temp_contadorDentes, (TickType_t) 10); //portMAX_DELAY
+	//xQueuePeek(xtempoUltimoDente, &temp_tempoUltimoDente, (TickType_t) 10);
+	//Prioridade normal
+
+	//int anguloCambota = ((temp_contadorDentes - 1) * anguloPorDente) + desvioPrimeiroDenteTDC;
+	int anguloCambota = ((contadorDentes - 1) * anguloPorDente) + desvioPrimeiroDenteTDC;
+
+	//Estimar o num de graus desde o ultimo dente
+	//long tempoDecorridoDesdeUltimoDente = micros() - temp_tempoUltimoDente;
+	long tempoDecorridoDesdeUltimoDente = micros() - tempoUltimoDente;
+	//tempoPorGrau = (temp_tempoUltimoDente / anguloPorDente); //Se n funcionar faço apartir da rpm
+	tempoPorGrau = (tempoUltimoDente / anguloPorDente); //Se n funcionar faço apartir da rpm
+	anguloCambota = anguloCambota + ldiv(tempoDecorridoDesdeUltimoDente, tempoPorGrau).quot;
+
+	if (anguloCambota > 360) {
+		anguloCambota = anguloCambota - 360;
+	}
+
+	return anguloCambota;
+}
+
+void getLowRPM(void) {
+
+	//Maxima prioridade ou desativar interrupcoes
+	//taskDISABLE_INTERRUPTS();
+	//tempoEntreDentes = (tempoUltimoDente - tempoPenultimoDente);
+	tempoEntreDentes = (tempoUltimoDente - tempoPenultimoDente);
+	uint64_t rpm_temporary = (((60000000 / (tempoEntreDentes * 36)) + 1) * 2); //+1 pq falha, n sei pk x2??
+	//taskENABLE_INTERRUPTS();
+	//Prioridade normal
+	xQueueSendToFront(xRPM, &rpm_temporary, 0);
+
+	//Serial.println(rpm_temporary);
+
 }
 
 // Interrupção gerada na transição do sinal do sensor de Hall
 void IRAM_ATTR onPulse(void) {
 
-	//Serial.println(falhaAtual);
 	BaseType_t xHigherPriorityTaskWoken;
 
 	tempoAtual = micros();
@@ -154,18 +240,15 @@ void IRAM_ATTR onPulse(void) {
 			contadorDentes++;
 			//Serial.print("Dente: ");
 			//Serial.println(contadorDentes);
-			xQueueSendToFrontFromISR(xContadorDentes, &contadorDentes,
-					&xHigherPriorityTaskWoken);
+			xQueueSendToFrontFromISR(xContadorDentes, &contadorDentes, &xHigherPriorityTaskWoken);
 		}
 
 		tempoPenultimoDente = tempoUltimoDente;
 		tempoUltimoDente = tempoAtual;
-		//xQueueSendToFrontFromISR(xtempoUltimoDente, &tempoUltimoDente, &xHigherPriorityTaskWoken);
+		xQueueSendToFrontFromISR(xtempoUltimoDente, &tempoUltimoDente, &xHigherPriorityTaskWoken);
 
-		//Serial.println(contadorrevolucoes);
+		//Serial.println(tempoUltimoDente);
 	}
-
-//Serial.println("int");
 }
 
 void vInitDisplay(void *pvParameters) {
@@ -181,10 +264,33 @@ void vInitDisplay(void *pvParameters) {
 		tft.setTextColor(ILI9341_WHITE);
 		tft.setTextSize(2);
 		tft.println(" Microcontroladores ");
-		tft.println(" Sistemas Embebidos ");
-		tft.println("     2016/2017      ");
 
+		if ( DEBUG == "ON" && DEBUG_LEVEL <= 2) {
+			Serial.print("Correu a task: ");
+			Serial.println(pcTaskGetTaskName(NULL));
+		}
 		vTaskDelay(500 / portTICK_PERIOD_MS);
+	}
+}
+
+void vGetMAP(void *pvParameters) {
+	while (true) {
+		uint8_t temp_valorAnalogico;
+		uint8_t temp_ddpAnalogica;
+		uint8_t temp_MAPValue;
+
+		temp_valorAnalogico = analogRead(MAPSensorPin);
+		temp_ddpAnalogica = temp_valorAnalogico * (VREF_PLUS - VREF_MINUS) / (pow(2.0, (float) ADC_RESOLUTION)) + VREF_MINUS;
+
+		temp_MAPValue = 45.2042 * temp_ddpAnalogica;
+
+		xQueueSendToFront(xMAP, &temp_MAPValue, 0);
+
+		if ( DEBUG == "ON" && DEBUG_LEVEL <= 2) {
+			Serial.print("Correu a task: ");
+			Serial.println(pcTaskGetTaskName(NULL));
+		}
+		vTaskDelay(200 / portTICK_PERIOD_MS);
 	}
 }
 
